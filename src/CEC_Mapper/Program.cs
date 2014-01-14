cf8739081b7e45de1bbf5cc2bc917cd745e952e0
billy-boy <billy-boy@markus-4ever.de>
2014-01-14 03:40:38 +0100

    Error detection for missing libCEC

--- src/CEC_Mapper/Program.cs
+++ src/CEC_Mapper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
@@ -15,10 +16,26 @@ namespace billy_boy.CEC_Mapper
  
         static void Main(string[] args)
         {
-            CEC_MapperThread.getInstance().Start();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            try
+            {
+                if (!File.Exists(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "LibCecSharp.dll")) || !File.Exists(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "libcec.dll")))
+                {
+                    if (log.IsWarnEnabled) log.Warn("libCEC DLLs are missing in base directory [" + System.AppDomain.CurrentDomain.BaseDirectory + "]");
+                    if (MessageBox.Show("It seems that there is no 'LibCecSharp.dll' and/or 'libcec.dll' inside the programs base directory.\r\nPlease have a look in the Readme before running this program...\r\n\r\nAre you shure to proceed? ", "HDMI CEC Mapper - libCEC missing", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
+                    {
+                        return;
+                    }
+                }
+                CEC_MapperThread.getInstance().Start();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frmMain());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("The application has crashed. Informations are below.\r\n\r\n"+ex.Message, "HDMI CEC Mapper - Application crash", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (log.IsErrorEnabled) log.Error("Application crashed. Reason: " + ex.Message + "");
+            }
         }
 
     }
